840129fa2cd7d0307b1573984fadfbf98b6c36b9
christosgkoutzis <christosgkoutzis@gmail.com>
2025-01-05 15:23:53 +0200

    Designed the first form of the base calculator (handles properly only positive values/results)

--- main.c
+++ main.c
@@ -18,12 +18,14 @@ typedef struct{
 
 basenumber getNumberAndBase(int basesLength);
 basenumber baseConverter(int supportedLength);
-basenumber baseCalculator(basenumber number1);
+basenumber baseCalculator(basenumber number1, int basesLength);
 int validBase(char situation[], int iterationCounter);
 bool baseCheck(int base, char inputNumber[]);
 char* lowerToUpper(char lowerString[]);
 char* convertToDecimal(int base, char inputNumber[]);
 char* convertFromDecimal(int base, char inputNumber[]);
+char validMathOperator(void);
+void switchPositions(int *number1, int *number2);
 
 
 /*
@@ -37,6 +39,8 @@ char* convertFromDecimal(int base, char inputNumber[]);
 const int MAX_CHARS = 32;
 const int SUPPORTED_BASES[] = {2,8,10,16};
 const char SUPPORTED_CHARS[][17] = {"01","01234567", "0123456789", "0123456789ABCDEF"};
+
+const char MATH_OPERATORS[4] = {'+','-','*','/'};
 
 int main(int argc, char *argv[]) {
 	printf("\n-------- / WELCOME TO C - BASEWIZARD \\ ------- \n");
@@ -60,7 +64,7 @@ int main(int argc, char *argv[]) {
 	result.value = malloc((MAX_CHARS + 2) * sizeof(char));
 	do {
 		if (cont == true) {
-			result = baseCalculator(result);
+			result = baseCalculator(result, supportedLength);
 		}
 		else{
 			switch(tool){
@@ -71,21 +75,25 @@ int main(int argc, char *argv[]) {
 					basenumber number1;
 					number1.value = malloc((MAX_CHARS + 2) * sizeof(char));
 					number1 = getNumberAndBase(supportedLength);
-					result = baseCalculator(number1);
+					result = baseCalculator(number1, supportedLength);
 					free(number1.value);
 			}
 		}	
 		printf("\nDo you want to use the calculator with the result? (press 0 to end or 1 to continue):\t");
 		scanf("%d", &cont);
+		// Clears the input buffer
+		int ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
 	} while (cont == true);
 	free(result.value);
 	return 0;
 }
 
 basenumber getNumberAndBase(int basesLength){
-	basenumber inputUpper;
 	// Maximum supported digits are 32 (+ \n from fgets and \0 char)
-	char input[MAX_CHARS + 2];
+	char* input = malloc((MAX_CHARS + 2) * sizeof(char));
+	basenumber inputUpper;
+	inputUpper.value = malloc((MAX_CHARS + 2) * sizeof(char));
 	bool compatibleLength, baseChecker;
 	do {
 		// Asks as input an integer
@@ -111,11 +119,12 @@ basenumber getNumberAndBase(int basesLength){
 			printf("\nThe current base does not match with the number you typed. Try again...\n");
 		}
 	} while(!baseChecker);
+	free(input);
 	return inputUpper;
 }
 
 
-// Checks if an inserted base is supported by the program
+// Checks if an inserted base is supported by the program and returns SUPPORTED_BASE index
 int validBase(char situation[], int iterationCounter){
 	// Asks for the integer's base (checks if it's supported)
 	int base, counter;
@@ -123,6 +132,9 @@ int validBase(char situation[], int iterationCounter){
 	do {
 		printf("\nType the %s base of the integer: \t", situation);
 		scanf("%d", &base);
+		// Clears the input buffer
+		int ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
 		for(counter = 0; counter < iterationCounter; counter++){
 			if(base == SUPPORTED_BASES[counter]){
 				isSupported = true;
@@ -289,8 +301,119 @@ char* convertFromDecimal(int base, char inputNumber[]){
 	return outputString;
 }
 
-basenumber baseCalculator(basenumber number1){
-	basenumber calcResult;
-	calcResult.value = malloc(MAX_CHARS * sizeof(char));
-	return calcResult;
+basenumber baseCalculator(basenumber number1, int basesLength){
+	basenumber number2 = getNumberAndBase(basesLength), calcResult;
+	char* number1ToDec;
+	char* number2ToDec;
+	int number1Int, number2Int, base10counter;
+	float resultFlo;
+	calcResult.value = malloc((MAX_CHARS + 2) * sizeof(char));
+	char operator = validMathOperator();
+	// Converts both string numbers to base10 format
+	if (SUPPORTED_BASES[number1.baseindex] != 10){
+		number1ToDec = convertToDecimal(number1.baseindex, number1.value);
 	}
+	if (SUPPORTED_BASES[number2.baseindex] != 10){
+		number2ToDec = convertToDecimal(number2.baseindex, number2.value);
+	}
+	// Converts to integers the base 2 numbers
+	number1Int = atoi(number1ToDec);
+	number2Int = atoi(number2ToDec);
+	// Performs the base10 operation regarding the operator's input
+	switch(operator){
+		case '+':
+			resultFlo = (float)(number1Int + number2Int);
+		break;
+		case '-':
+			switchPositions(&number1Int, &number2Int);
+			resultFlo = (float)(number1Int - number2Int);
+		break;
+		case '*':
+			resultFlo = (float)(number1Int * number2Int);
+		break;
+		case '/':
+			switchPositions(&number1Int, &number2Int);
+			if(number2Int == 0){
+				printf("Calculation is impossible. Exiting program...");
+				exit(6);
+			}
+			resultFlo = (float)(number1Int / number2Int);
+			break;
+	}
+	// Copies as a string the float value of the number to calcResult
+	sprintf(calcResult.value,"%.2f",resultFlo);
+	// Fills baseindex with base10 counter
+	for (base10counter = 0; base10counter < basesLength; base10counter++){
+		if(SUPPORTED_BASES[base10counter] == 10){
+			break;
+		}
+	}
+	calcResult.baseindex = base10counter;
+	int convertInput = 0;
+	bool isValidInput;
+	printf("\n-------- | Integer Base Calculator Results | ------- \n\nOperation:\t%s (base %d) %c %s (base %d) = %s (base 10)\n", number1.value, SUPPORTED_BASES[number1.baseindex], operator, number2.value, SUPPORTED_BASES[number2.baseindex], calcResult.value);
+	do {
+		printf("\nDo you want to convert it to another base (only the integer part will be converted for divisions)?(press 0 for no or 1 for yes)\n\nAnswer:\t");
+		scanf("%d", &convertInput);
+		// Clears the input buffer
+		int ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
+		isValidInput = (convertInput == 0 || convertInput == 1);
+		if (!isValidInput){
+			printf("\nIncorrect input. Please try again...");
+		}
+	} while (!isValidInput);
+	if (convertInput == true) {
+		calcResult.baseindex = validBase("next", basesLength);
+		strcpy(calcResult.value, convertFromDecimal(calcResult.baseindex, calcResult.value));
+		printf("\nNew integer value: \t%s\nNew Base: \t%d", calcResult.value, SUPPORTED_BASES[calcResult.baseindex]);
+	}
+	free(number2.value);
+	free(number1ToDec);
+	free(number2ToDec);
+	return calcResult;
+}
+
+char validMathOperator(void){
+	int i;
+	char mathOperator;
+	bool isValidOperator = false;
+	do {
+	printf("\nType an operator(+,-,*,/):\t");
+	scanf("%c", &mathOperator);
+	// Clears the input buffer
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF);
+	for(i = 0; i < 4; i++){
+		if(MATH_OPERATORS[i] == mathOperator){
+			isValidOperator = true;
+		}
+	}
+	if (!isValidOperator){
+		printf("\nIncorrect operator input. Try again...");
+	}
+	} while(!isValidOperator);
+	return mathOperator;
+}
+
+void switchPositions(int *number1, int *number2){
+	int switchSelect;
+	bool isValidInput;
+	do{
+		printf("\nThe current position before the operation is:\n\tNumber 1:\t%d\n\tNumber 2: \t%d\nDo you want to switch positions(press 0 for no and 1 for yes)?\t", *number1, *number2);
+		scanf("%d", &switchSelect);
+		// Clears the input buffer
+		int ch;
+		while ((ch = getchar()) != '\n' && ch != EOF);
+		isValidInput = (switchSelect == 0 || switchSelect == 1);
+		if (!isValidInput){
+			printf("\nIncorrect input. Please try again...");
+		}
+	} while (!isValidInput);
+	if(switchSelect){
+		int temp = *number1;
+		*number1 = *number2;
+		*number2 = temp;
+	}
+	return;
+}
